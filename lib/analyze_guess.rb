ed3d6dc17823e6aa2ba9fb6ce3e8146a9205cf77
Kelly <kellysteensma@gmail.com>
2013-01-22 19:25:08 -0600

    GOT VARIABLES TO WORK. WAHOO WAHOO.

--- lib/analyze_guess.rb
+++ lib/analyze_guess.rb
@@ -1,11 +1,19 @@
 class AnalyzeGuess
+	
 	attr_accessor :guess, :code
+
+	def initialize(code, guess)
+		@guess = guess
+		@code = code
+	end
 
 	def response
 		if @guess == @code
 			["b","b","b","b","b"]
-		elsif @guess - @code != []
+		elsif (@guess - @code) != []
 			["","","","",""]
+		elsif (@guess - @code) == []
+			["w","w","w","w","w"]
 		end
 	end
 
--- spec/analyze_guess_spec.rb
+++ spec/analyze_guess_spec.rb
@@ -3,16 +3,27 @@ require './lib/analyze_guess'
 	describe AnalyzeGuess do
 	
 		describe '#response' do
-	
-	    before  { subject.response }
 
-      let(:code) { ["g","g","g","g","g"] }
-    	let(:guess) { ["g","g","g","g","g"] }    
-    	its(:response) { should == ["b","b","b","b","b"] }
+			subject 				{ AnalyzeGuess.new(code, guess) }
+	    before  				{ subject.response }
 
-      let(:code) { ["r","r","r","r","r"] }
-    	let(:guess) { ["g","g","g","g","g"] }
-    	its(:response) { should == ["","","","",""] }
+      context 'when code == guess' do
+	      let(:code) 			{ ["g","g","g","g","g"] }
+	    	let(:guess) 		{ ["g","g","g","g","g"] }    
+	    	its(:response) 	{ should == ["b","b","b","b","b"] }
+    	end
+
+	    context 'when whole code != whole guess' do
+	      let(:code) 			{ ["r","r","r","r","r"] }
+	    	let(:guess) 		{ ["g","g","g","g","g"] }    
+	    	its(:response) 	{ should == ["","","","",""] }
+	    end
+
+	    context 'when whole code is a near match to whole guess' do
+	      let(:code) 			{ ["g","g","y","r","r"] }
+	    	let(:guess) 		{ ["r","r","g","g","y"] }    
+	    	its(:response) 	{ should == ["w","w","w","w","w"] }
+	    end
 
 		end
 	end
